44764dd36ead9def3a263f2d57444ad010975566
Yaya Mamoudou <57509871+yaya-mamoudou@users.noreply.github.com>
2023-12-24 08:50:16 +0100

    +

--- posibleCombo.js
+++ posibleCombo.js
@@ -1,8 +1,9 @@
-let combos = {
+const combos = {
     '2': ['a', 'b', 'c'], '3': ['d', 'e', 'f'], '4': ['g', 'h', 'i'],
     '5': ['j', 'k', 'l'], '6': ['m', 'n', 'o'], '7': ['p', 'q', 'r', 's'],
     '8': ['t', 'u', 'v'], '9': ['w', 'x', 'y', 'z']
 }
+
 function posibleCombo(input) {
     if (typeof input !== 'string') return 'Invalid input';
     if (input.length > 2) return 'Invalid length';
@@ -12,7 +13,7 @@ function posibleCombo(input) {
 
     if (hasWrongKeys) return 'Invalid key(s) found! Only 2-9 allowed';
 
-    let result = []
+    let result = [];
 
     combos[letters[0]].forEach(element => {
         if (letters[1]) {
@@ -28,5 +29,4 @@ function posibleCombo(input) {
 }
 
 let result = posibleCombo('93');
-
 console.log(result);
